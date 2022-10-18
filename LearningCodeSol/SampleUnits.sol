88b691e2a504335a54cc491eaa73e62d73f2d66a
Cesar Arcos-Gonzalez <60860385+CAG9@users.noreply.github.com>
2022-10-17 22:17:30 -0500

    Create SampleUnits.sol

---
+++ LearningCodeSol/SampleUnits.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+
+contract SampleUnits {
+    modifier betweenOneAndTwoEth(){
+        require(msg.value >= 1 ether && msg.value <= 2 ether );
+        _; 
+    } 
+
+    uint runUntilTimestamp;
+    uint startTimestamp;
+
+    constructor(uint startInDays){
+        startTimestamp = block.timestamp + (startInDays * 1 days);
+        runUntilTimestamp = startTimestamp + (7 days);
+    }
+
+
+}
